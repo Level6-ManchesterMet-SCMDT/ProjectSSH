c46148dbfca06f5081ab01efd75725cc6f3f3db9
Federico D'Apote <44197324+notthereverseflash@users.noreply.github.com>
2021-02-15 20:33:10 +0000

    Adding gunpowder smell ability to shop

--- Assets/Scripts/Abilities/Abilities.cs
+++ Assets/Scripts/Abilities/Abilities.cs
@@ -5,28 +5,41 @@ using Photon.Pun;
 
 public class Abilities : MonoBehaviourPunCallbacks
 {
+    [Header("General")]
 
-    [SerializeField] GameObject GunpowderAbility;
     [SerializeField] GameObject Gun;
-    [SerializeField] Camera Camera;
+    [SerializeField] GameObject Camera;
 
-    public bool GunpowderAbilityPurchased = false;
+    //Gunpowder Smell
+    [Header("Gunpowder Smell")]
+
+    [SerializeField] GameObject GunpowderAbilityEffect;
+
     public int GunpowderAbilitypower = 1;
+    public bool GunpowderAbilityUpgraded = false;
+
+    //Gunpowder Smell
+
+    void Update()
+    {
+        if (GunpowderAbilityUpgraded)
+        {
+            Debug.Log("Test");
+            //Camera.GetComponent<Camera>().cullingMask = (1 << LayerMask.NameToLayer("Ground") | (1 << LayerMask.NameToLayer("GunpowderEffect")) | (1 << LayerMask.NameToLayer("UI")) | (1 << LayerMask.NameToLayer("Default")));
+            Camera.GetComponent<Camera>().cullingMask ^= 1 << LayerMask.NameToLayer("GunpowderEffect");
+        }
+    }
+
+    public void GunpowderUpgraded()
+    {
+        this.photonView.RPC("GunpowderUpgraded_RPC", RpcTarget.All);
+    }
 
     public void ShootEffect()
     {
-        GameObject GunpowderEffect = PhotonNetwork.Instantiate(GunpowderAbility.name, Gun.transform.position, this.transform.rotation, 0);
+        GameObject GunpowderEffect = PhotonNetwork.Instantiate(GunpowderAbilityEffect.name, Gun.transform.position, this.transform.rotation, 0);
         StartCoroutine(respawnWait(GunpowderEffect, this.transform.GetComponent<Abilities>().GunpowderAbilitypower));
         GunpowderEffect.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (this.photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Camera.GetComponent<Camera>().cullingMask = (1 << LayerMask.NameToLayer("Ground") | (1 << LayerMask.NameToLayer("GunpowderEffect")) | (1 << LayerMask.NameToLayer("UI")) | (1 << LayerMask.NameToLayer("Default")));
-        }
     }
 
     IEnumerator respawnWait(GameObject GunpowderEffect, int GunpowderAbilitypower)
@@ -34,5 +47,12 @@ public class Abilities : MonoBehaviourPunCallbacks
         //Wait for 4 seconds
         yield return new WaitForSeconds(5 * GunpowderAbilitypower);
         PhotonNetwork.Destroy(GunpowderEffect);
+    }
+
+    [PunRPC]
+    public void GunpowderUpgraded_RPC()
+    {
+        GunpowderAbilitypower++;
+        GunpowderAbilityUpgraded = true;
     }
 }
